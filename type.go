fb3654eb36a2980e929f95c5ff78d8aee37a41e1
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-04-15 04:39:14 +0900

    fixed type.go typed the constant

--- type.go
+++ type.go
@@ -1,28 +1,28 @@
 package main
 
 const (
-	EthTypeIpv4 	= 0x0800
-	EthTypeArp  	= 0x0806
-	EthTypeAppTalk	= 0x809b
-	EthTypeVlan		= 0x8100
-	EthTypeIpx		= 0x8137
-	EthTypeIpv6		= 0x86dd
+	EthTypeIpv4    uint16 = 0x0800
+	EthTypeArp     uint16 = 0x0806
+	EthTypeAppTalk uint16 = 0x809b
+	EthTypeVlan    uint16 = 0x8100
+	EthTypeIpx     uint16 = 0x8137
+	EthTypeIpv6    uint16 = 0x86dd
 
-	ProtoTypeIcmp	= 0x01
-	ProtoTypeTcp	= 0x06
-	ProtoTypeUdp	= 0x11
+	ProtoTypeIcmp uint16 = 0x01
+	ProtoTypeTcp  uint16 = 0x06
+	ProtoTypeUdp  uint16 = 0x11
 
-	NxtHeadIcmp6	= 0x3a
-	NxtHeadTcp		= 0x06
-	NxtHeadUdp		= 0x11
+	NxtHeadIcmp6 uint16 = 0x3a
+	NxtHeadTcp   uint16 = 0x06
+	NxtHeadUdp   uint16 = 0x11
 
-	IcmpEchoReply	= 0x00
-	IcmpDstUnreach	= 0x03
-	IcmpRedirect	= 0x05
-	IcmpEchoReq		= 0x08
-	IcmpExceeded	= 0x0b
+	IcmpEchoReply  uint16 = 0x00
+	IcmpDstUnreach uint16 = 0x03
+	IcmpRedirect   uint16 = 0x05
+	IcmpEchoReq    uint16 = 0x08
+	IcmpExceeded   uint16 = 0x0b
 
-	Icmp6DstUnreath	= 0x01
-	Icmp6Echoreq	= 0x80
-	Icmp6Echoreply	= 0x81
+	Icmp6DstUnreath uint16 = 0x01
+	Icmp6Echoreq    uint16 = 0x80
+	Icmp6Echoreply  uint16 = 0x81
 )
