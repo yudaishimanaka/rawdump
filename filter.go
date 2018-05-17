c31b908354cbd7e645e4c6838e29c25747c592a9
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-05-17 22:47:41 +0900

    fixed libbpf

--- filter.go
+++ filter.go
@@ -15,10 +15,33 @@ package main
 #cgo windows CFLAGS: -I C:/WpdPack/Include
 #cgo windows,386 LDFLAGS: -L C:/WpdPack/Lib -lwpcap
 #cgo windows,amd64 LDFLAGS: -L C:/WpdPack/Lib/x64 -lwpcap
-#include "libbpf/cbpf.h"
-#include "libbpf/bpf_load.h"
-#include "libbpf/ebpf.h"
-#include "libbpf/pcap_helpers.h"
-#include "libbpf/utils.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <libbpf/cbpf.h>
+#include <libbpf/bpf_load.h>
+#include <libbpf/ebpf.h>
+#include <libbpf/pcap_helpers.h>
+#include <libbpf/utils.h>
+struct sock_fprog cbpf = {0};
 */
 import "C"
+
+import (
+	"unsafe"
+)
+
+func SetBPF(expr, ifname string) error {
+	ccbpf := *C.cbpf
+	cexpr := C.CString(expr)
+	cifname := C.CString(ifname)
+	cbool := C.bool(0)
+	defer C.free(unsafe.Pointer(cexpr))
+	defer C.free(unsafe.Pointer(cifname))
+	defer C.free(unsafe.Pointer(cbool))
+
+	C.filter_try_compile(cexpr, ccbpf, C.dev_get_iftype(cifname))
+
+	C.cbpf_dump_all(ccbpf, cbool)
+
+	return nil
+}
